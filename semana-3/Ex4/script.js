671cb5be0fecc6b5aa9efa984c97cdddab264ff8
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-02-22 15:06:00 -0300

    exrcicios

--- semana-3/Ex4/script.js
+++ semana-3/Ex4/script.js
@@ -1,15 +1,17 @@
 let valorPedido = parseFloat(prompt("Digite valor do pedido:"))
 
+
+
 if(valorPedido < 10000.00){
     console.log("Classificação do pedido BRONZE !")
     alert("Classificação do pedido BRONZE !")
-}else if(valorPedido >= 10000.00 && valorPedido <= 50000.00){
+}else if(valorPedido >= 10000.00 && valorPedido < 100000.00){
     console.log("Classificação do pedido PRATA !")
     alert("Classificação do pedido PRATA !")
-}else if(valorPedido >= 100000.00 && valorPedido <= 500000.00){
+}else if(valorPedido >= 100000.00 && valorPedido < 500000.00){
     console.log("Classificação do pedido OURO !")
     alert("Classificação do pedido OURO !")
-}else if(valorPedido > 500000.00 ){
+}else if(valorPedido >= 500000.00 ){
     console.log("Classificação do pedido PLATINUM !")
     alert("Classificação do pedido PLATINUM !")
 }
