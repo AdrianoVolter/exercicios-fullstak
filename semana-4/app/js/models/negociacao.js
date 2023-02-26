c605f95713de5a6643bfa4ef4df40a0da0f3ecc3
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-02-26 18:03:25 -0300

    Exercício 3 semana 4 v.1.0

---
+++ semana-4/app/js/models/negociacao.js
@@ -0,0 +1,21 @@
+class Negociacao{
+    constructor(data, quantidade, valor){
+
+        this.data = data
+        this.quantidade = quantidade
+        this.valor = valor
+    }
+    dataA(){
+        return this.data
+    }
+    
+    quantidadeA(){
+        return this.quantidade
+    }
+    valorA(){
+        return this.valor
+    }
+
+
+}
+
