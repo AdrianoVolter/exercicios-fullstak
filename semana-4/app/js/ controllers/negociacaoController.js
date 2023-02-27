c67d4f2df8b8ac7304f8f1ae662f522abae94989
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-02-26 22:35:53 -0300

    Exercício 4 semana 4 v.1.0

---
+++ semana-4/app/js/ controllers/negociacaoController.js
@@ -0,0 +1,13 @@
+ class NegociacaoController{
+    constructor(){
+        this.campoData = document.getElementById("data");
+        this.campoQuantidade  = document.getElementById("quantidade");
+        this.campoValor = document.getElementById("valor");
+    
+    
+
+    }
+    
+
+}
+
