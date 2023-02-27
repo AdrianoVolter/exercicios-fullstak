cb4124a87558b6c617e70a1932b8e883b99b9fdb
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-02-27 08:36:13 -0300

    Exercício 7 semana 4 v.1.0

--- semana-4/app/js/controllers/negociacaoController.js
+++ semana-4/app/js/controllers/negociacaoController.js
@@ -2,23 +2,27 @@ import { Negociacao } from "../models/negociacao.js";
 import { ListaNegociacoes } from "../models/listaNegociacoes.js";
 export class NegociacaoController{
 
+    #campoData;
+    #campoQuantidade;
+    #campoValor;
+    #negociacoes = new ListaNegociacoes();
+
     constructor(){
 
-        this.campoData = document.getElementById("data");
-        this.campoQuantidade  =document.getElementById("quantidade");
-        this.campoValor = document.getElementById("valor");
+        this.#campoData = document.getElementById("data");
+        this.#campoQuantidade  =document.getElementById("quantidade");
+        this.#campoValor = document.getElementById("valor");
 
     }
 
     criarNegociacao(){
 
-        let data = this.campoData;
-        let quantidade = this.campoQuantidade.value;
-        let valor =this.campoValor.value;
-
+        let data = this.#campoData;
+        let quantidade = this.#campoQuantidade.value;
+        let valor =this.#campoValor.value;
         let negocio = new Negociacao(data, quantidade, valor);
-        
-        console.log(negocio);
+        this.#negociacoes.adcicionar(negocio)
+        console.log(this.#negociacoes.negociacoes);
             
     };
 
