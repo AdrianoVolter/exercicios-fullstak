b63f1c8b550f6ad878f86ff1bfcd0be551467e8f
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-01 11:35:14 -0300

    Semana 5, exercicio 4 V.1.1

--- semana-5/ex-4/script.js
+++ semana-5/ex-4/script.js
@@ -1,11 +1,10 @@
 var arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrayInvertido = [];
+
+
 console.log(arrayNumeros);
 
-// arrayInvertido = arrayNumeros.map(()=>{
-//     arrayNumeros.reverse()
-    
-// })
+arrayInvertido = arrayNumeros.reverse()
 
-// console.log(arrayInvertido);
+console.log(arrayInvertido)
 
