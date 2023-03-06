93fef3a95cb835df0d5212190a3dca9fb476abd1
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-06 14:49:09 -0300

    Exercício 4 s05 V.1.1

--- semana-5/ex-4/script.js
+++ semana-5/ex-4/script.js
@@ -1,10 +1,6 @@
-var arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrayInvertido = [];
 
 
-console.log(arrayNumeros);
-
-arrayInvertido = arrayNumeros.reverse()
-
-console.log(arrayInvertido)
-
+const impares = numeros => numeros.filter(x => x % 2 != 0);
+console.log()
