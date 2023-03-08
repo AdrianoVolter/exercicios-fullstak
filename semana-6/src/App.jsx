cd25a9b59056f5242d9dd26106ea36194c724585
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-08 17:59:48 -0300

    Exercício semana 6 V.1.4

--- semana-6/src/App.jsx
+++ semana-6/src/App.jsx
@@ -1,17 +1,31 @@
 import './App.css'
-import Card from "./components/Card"
 
 function App() {
+  //const [count, setCount] = useState(0)
 
-  //const urlFerrari = "https://s2.glbimg.com/WwkvfLhK4mt40-5fkdlWSHbyJqo=/0x0:1280x921/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/M/0/zLTv6fTfuWwIoCbUdZXA/ferrari-sf90-stradale-2020-1280-05.jpg" 
-  //const h_titulo = "Carro Ferrari"
   return (
     <div className="App">
-      <Card nome="Adriano Jose Volter" idade="38 anos" imagem="https://avatars.githubusercontent.com/u/91354299?s=96&v=4" 
-   width={200} iconGithub="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAZdJREFUSEu1lYExBVEMRe+vABWgAlSAClABKkAFqAAdUMGnAlSADuiACpgzk+zkZ7L735rZzOzs7puXe5P7kryZJrbZxPhqITiUdCBp2x5ierfnRdLDUJBDBADfSNpYkuWnpAtJj9W+PoIrSZcj5bs1ogW3iuA/4A56J+k8MmSCPUnPtuFJEmQnks5s7cPeW/ZG/3vbc2xrR1GuTICe67bx2gj4XZX0nSSLazFrMDZ9byTgUOcBhOiIvsWyrKeW2UKZkqqnCei+JMqwxSjhtyq4mAFgu0FrnMZY9KdPdnCOBGi8YoivkjjwMRYJOuxI8BvQJiGIEZDN2pjwrcpcgS+fADGDfMi0P93ZYjQXY8Wtq8C+Mv2x86D86M7cAw5EL1B5OZCyTHGiSUiTEiUqL1u6mp5wIoDJmKrjO1onT64i/n1UAESZOUjsagfLkvr6Qv9Uw871LKdjCLUaikuHnfs7CQ1DNj7UohSZoMpy8EZDLmRg+FXOToDmnE85VlquTIjIgmyiMUq47cqbzDe2ELT0Qe+eyQn+AIklVhnz1DvpAAAAAElFTkSuQmCC"/>
-  
+      <div>
+        <a href="" target="_blank">
+          <img src="https://avatars.githubusercontent.com/u/91354299?v=4" className="logo" alt="github" />
+        </a>
+        {/* <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h3>Adriano Jose Volter</h3>
+      <div className="card">
+        <p >
+          38 anos
+        </p>
+        {/* <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
+      </div>
+      <p className="read-the-docs">
+        Aprendendo React
+      </p>
     </div>
-    
   )
 }
 
