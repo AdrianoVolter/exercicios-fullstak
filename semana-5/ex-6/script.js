2f7ce1c418fa2d8a6e78effc5ff5f191754a736a
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-08 09:14:59 -0300

    Exercicio 6 S05

---
+++ semana-5/ex-6/script.js
@@ -0,0 +1,9 @@
+var frutas = [ "Banana", "Morango", "Maçâ", "Uva", "Pêra"];//[“Banana”, “Morango”, “Maçã”, “Uva”, “Pêra” ]
+
+ //frutas.forEach =(el) =>{
+    
+localStorage.setItem("Frutas", JSON.stringify(frutas));
+ 
+ //}
+
+console.log(frutas)
