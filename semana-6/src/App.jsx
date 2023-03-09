d508c61de7f85f74ef102abd60d18d7943ff96e2
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-09 00:18:45 -0300

    Exercício semana 6 V.1.6

--- semana-6/src/App.jsx
+++ semana-6/src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
 import Card from './components/Card'
-
+import 'boxicons'
 function App() { 
   const img = "https://avatars.githubusercontent.com/u/91354299?v=4"
   const no = "Adriano Jose Volter"
   const id= "38"
 
+
   return(
-    
-   <Card  
-   imagem={img}
-   nome= {no}
-   idade= {id}
-   />
+    <div className='geral'>
+       <Card  
+        imagem={img}
+        nome= {no}
+        idade= {id}
+        />
+
+        
+    </div>
+  
   )
 }
 
--- semana-6/src/components/Card.jsx
+++ semana-6/src/components/Card.jsx
@@ -1,14 +1,17 @@
+import './Card.css'
 function Card(props){
 
     return(
 
-        <div>
-            <div>
+        <div className="caixa">
+            <div className="imagem">
                 <img src={props.imagem} alt="github" />
             </div>
-            <div>
+            <div className='info'>
                 <h2>Nome : {props.nome}</h2>
-                <p>Idade : {props.idade}</p>
+                <p>Idade : {props.idade} anos</p>
+                 <box-icon  type='logo' name='github'></box-icon>
+                 <box-icon type='logo' name='linkedin-square'></box-icon>
             </div>
         </div>
 
