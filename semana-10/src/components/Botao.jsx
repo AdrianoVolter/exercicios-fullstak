d9fe14ee3e3db8a1dc0b72ea73e4f62f0f69dc89
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-05 16:08:37 -0300

    outros , GlobalStyle

--- semana-10/src/components/Botao.jsx
+++ semana-10/src/components/Botao.jsx
@@ -1,9 +1,14 @@
 import { SCButton2 } from "./Button.style"
 
 export default function Botao(props) {
+    // const props = {
+    //     backgroundColor: btnProps.backgroundColor || "#000",
+    //     children: btnProps.children
+
+    // }
   return (
     <SCButton2
-      backgroundColor={props.backgroundColor}
+      backgroundColor={props.backgroundColor || "#000"}
     >
       {props.children}
     </SCButton2>
